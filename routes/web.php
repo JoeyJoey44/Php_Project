73384c06e171c6a8b864710e21e91ec67854ab19
JoeyJoey44 <joeythejoey2121@gmail.com>
2025-03-26 20:42:53 -0700

    Added a basic UI template for us to work off of.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Welcome.blade.php Route
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome.index');
+
+// Features.blade.php Route
+Route::get('/features', function () {
+    return view('features');
+})->name('features.index');
+
+// Faq.blade.php Route
+Route::get('/faq', function () {
+    return view('faq');
+})->name('faq.index');
+
+// About.blade.php Route
+Route::get('/about', function () {
+    return view('about');
+})->name('about.index');
